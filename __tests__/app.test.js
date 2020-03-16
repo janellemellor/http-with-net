126b9f0aece18da6922da7a53b8ccc77a5db3995
janelle mellor <janellemellor@gmail.com>
2020-03-16 16:50:29 -0700

    add example red route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,17 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
-
+describe('app routes', () => {
+  it('retrieves the red html page', () => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual(`
+<html>
+    <body>
+        <h1>This is red<h1>
+    </body>
+</html>`);
+      });
+  });
 });
