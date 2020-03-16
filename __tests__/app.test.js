d6b74ec7e18f912e4d1cf6bebeca6daa2c2f1fca
janelle mellor <janellemellor@gmail.com>
2020-03-16 16:57:08 -0700

    add hi get route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,16 +2,31 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('retrieves the hi html page', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual(`
+            <html>
+                <body>
+                    hi
+                </body>
+            </html>`);
+      });
+  });
+  
   it('retrieves the red html page', () => {
     return request(app)
       .get('/red')
       .then(res => {
         expect(res.text).toEqual(`
-<html>
-    <body>
-        <h1>This is red<h1>
-    </body>
-</html>`);
+            <html>
+                <body>
+                    <h1>This is red<h1>
+                </body>
+            </html>`);
       });
   });
+  
 });
+
