2aad1b3a40876abe368fe297bfd73348f0230919
janelle mellor <janellemellor@gmail.com>
2020-03-16 18:26:14 -0700

    fix plain text routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,17 +1,20 @@
 const request = require('supertest');
-const app = require('../lib/app');
+const app = require('../lib/app.js');
 
 describe('app routes', () => {
-  it('retrieves the hi html page', () => {
+  it('retrieves the hi plain text page', () => {
     return request(app)
       .get('/')
       .then(res => {
-        expect(res.text).toEqual(`
-            <html>
-                <body>
-                    hi
-                </body>
-            </html>`);
+        expect(res.body).toEqual('hi');
+      });
+  });
+
+  it('creates a plain text page', () => {
+    return request(app)
+      .post('/echo')
+      .then(res => {
+        expect(res.body).toEqual('is there an echo in here?');
       });
   });
   
